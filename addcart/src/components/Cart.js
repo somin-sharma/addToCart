223107c6ec15009788ac676e82adaa1413986fdd
somin-sharma <sominsharma0000@gmail.com>
2023-08-09 23:24:16 +0530

    y

--- addcart/src/components/Cart.js
+++ addcart/src/components/Cart.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './cart.css'
+import Items from './Items'
+import {products} from "./products"
 
 const Cart = () => {
+    const [item, setItem] = useState(products);
   return (
     <div>
         <header>
@@ -21,108 +24,19 @@ const Cart = () => {
             <p className='total-items'>you have <span className='total-items-count'>7</span> items in shopping cart </p>
             <div className='class-items'>
                 <div className='cart-items-container'>
-                    <div className='items-info'>
-                        <div className='product-img'>
-                            <img src='https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt='image'/>
-                        </div>
-                        <div className='title'>
-                            <h2>Samsung S21</h2>
-                            <p>color black</p>
-                        </div>
-                        <div className='add-minus-quantity'>
-                        <i class="fas fa-minus minus"></i>
-                        <input type='text' placeholder='2'/>
-                        <i class="fas fa-plus add"></i>
-                        </div>
-                        <div className='price'>
-                            <h3>2000rs</h3>
-                        </div>
-
-                        <div className='remove-item'>
-                        <i class="fas fa-trash-alt remove"></i>
-                        </div>
-                    </div>
-
-                    <hr/>
-
-                    <div className='items-info'>
-                        <div className='product-img'>
-                            <img src='https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt='image'/>
-                        </div>
-                        <div className='title'>
-                            <h2>Samsung S21</h2>
-                            <p>color black</p>
-                        </div>
-                        <div className='add-minus-quantity'>
-                        <i class="fas fa-minus minus"></i>
-                        <input type='text' placeholder='2'/>
-                        <i class="fas fa-plus add"></i>
-                        </div>
-                        <div className='price'>
-                            <h3>2000rs</h3>
-                        </div>
-
-                        <div className='remove-item'>
-                        <i class="fas fa-trash-alt remove"></i>
-                        </div>
-                    </div>
-
-                    <hr/>
-
-                    <div className='items-info'>
-                        <div className='product-img'>
-                            <img src='https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt='image'/>
-                        </div>
-                        <div className='title'>
-                            <h2>Samsung S21</h2>
-                            <p>color black</p>
-                        </div>
-                        <div className='add-minus-quantity'>
-                        <i class="fas fa-minus minus"></i>
-                        <input type='text' placeholder='2'/>
-                        <i class="fas fa-plus add"></i>
-                        </div>
-                        <div className='price'>
-                            <h3>2000rs</h3>
-                        </div>
-
-                        <div className='remove-item'>
-                        <i class="fas fa-trash-alt remove"></i>
-                        </div>
-                    </div>
-
-                    <hr/>
-
-                    <div className='items-info'>
-                        <div className='product-img'>
-                            <img src='https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt='image'/>
-                        </div>
-                        <div className='title'>
-                            <h2>Samsung S21</h2>
-                            <p>color black</p>
-                        </div>
-                        <div className='add-minus-quantity'>
-                        <i class="fas fa-minus minus"></i>
-                        <input type='text' placeholder='2'/>
-                        <i class="fas fa-plus add"></i>
-                        </div>
-                        <div className='price'>
-                            <h3>2000rs</h3>
-                        </div>
-
-                        <div className='remove-item'>
-                        <i class="fas fa-trash-alt remove"></i>
-                        </div>
-                    </div>
-
                     
+                    {
+                        item.map((curItem)=> {
+                            return <Items key={curItem.id} {...curItem}/>
 
+                        })
+                    }
 
-
-
+                    <Items/>
                 </div>
                 <div className='card-total'>
                     <h3> Cart Total : <span>2200rs</span></h3>
+                    <button>Checkout</button>
                 </div>
 
             </div>
--- addcart/src/components/Items.js
+++ addcart/src/components/Items.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
-const Items = () => {
+const Items = ({title, description, price, img, quantity}) => {
   return (
-    <div>Items</div>
+    <div>
+        <div className='items-info'>
+                        <div className='product-img'>
+                            <img src={img} alt='image'/>
+                        </div>
+                        <div className='title'>
+                            <h2>{title}</h2>
+                            <p>{description}</p>
+                        </div>
+                        <div className='add-minus-quantity'>
+                        <i class="fas fa-minus minus"></i>
+                        <input type='text' placeholder='2'/>
+                        <i class="fas fa-plus add"></i>
+                        </div>
+                        <div className='price'>
+                            <h3>{price}</h3>
+                        </div>
+
+                        <div className='remove-item'>
+                        <i class="fas fa-trash-alt remove"></i>
+                        </div>
+                    </div>
+
+                    <hr/>
+
+    </div>
   )
 }
 
